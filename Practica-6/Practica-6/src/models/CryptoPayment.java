bd5ebe5f1f286369ecee8565cc32ae4365f1cd46
ERICK-RAMOS <erickramossucullani@gmail.com>
2026-04-30 20:37:46 -0400

    Add files via upload

---
+++ Practica-6/Practica-6/src/models/CryptoPayment.java
@@ -0,0 +1,23 @@
+package models;
+
+import core.PaymentMethod;
+
+public class CryptoPayment extends PaymentMethod {
+    String walletAddress;
+    public CryptoPayment(String holderName, String walletAddress) {
+        super(holderName);
+        this.walletAddress = walletAddress;
+    }
+
+    @Override
+    public boolean processPayment(double amount) {
+        if (amount < 10.0) {
+            System.out.println("Crypto payment failed: Amount must be at least $10.0");
+            return false;
+        }
+        System.out.println("Processing crypto payment to wallet: " + walletAddress);
+        generateReceipt(amount);
+        return true;
+    }
+
+}
