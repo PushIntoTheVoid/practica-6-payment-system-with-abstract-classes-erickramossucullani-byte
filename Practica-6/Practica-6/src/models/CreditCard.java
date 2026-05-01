175c494de22649b46ee4f6b66fbf05e293ea6b9b
ERICK-RAMOS <erickramossucullani@gmail.com>
2026-04-30 21:09:52 -0400

    Add files via upload

---
+++ Practica-6/Practica-6/src/models/CreditCard.java
@@ -0,0 +1,21 @@
+package models;
+
+import core.PaymentMethod;
+
+public class CreditCard extends PaymentMethod{
+    private String cardNumber;
+
+    public CreditCard(String holderName, String cardNumber){
+        super(holderName);
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public boolean processPayment(double amount){
+
+        System.out.println("Processing payment with Credit Card ending in " + cardNumber.substring(cardNumber.length() - 4));
+        generateReceipt(amount);
+        return true;
+    }
+
+}
